3f1037f72ce9fb8d42ed883ea115d8e8cf0b56af
Devdeepakjha <dj145740@gmail.com>
2026-04-14 10:19:56 +0530

    fixed app for deployment

--- app.py
+++ app.py
@@ -8,12 +8,14 @@ from routes.admin_routes import admin_bp
 app = Flask(__name__)
 app.secret_key = "simple_library_secret"
 
+# Register Blueprints
 app.register_blueprint(auth_bp)
 app.register_blueprint(book_bp)
 app.register_blueprint(admin_bp)
 
+# Initialize DB (safe for deployment)
+init_db()
 
+# Run locally
 if __name__ == "__main__":
-    init_db()
-    app.run(debug=True)
-
+    app.run()
